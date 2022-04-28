aeb6e99e4b667ccb0f94a253a5fde23ce220a0df
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-04-28 17:34:44 -0400

    Finished routes, removed some unneed logic

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,10 +30,6 @@ router.get('/:id', async (req, res) => {
       ]
     });
 
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
       res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -53,13 +49,16 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update(req.params.id);
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
+    const categoryData = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
     }
-
+  );
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -76,11 +75,6 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
 
     res.status(200).json(categoryData);
   } catch (err) {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -39,10 +39,6 @@ router.get('/:id', async (req, res) => {
         }
       ]
     });
-
-    if (!productData) {
-      res.status(404).json({message: 'No product found with that id!'})
-    }
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err)
